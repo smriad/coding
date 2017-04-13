1fb44dfedc7b517f8595fc81483a9a3be7313da6
S.M. Riad <smriad@gmail.com>
2017-04-13 19:00:19 +0600

    initial commit

---
+++ test.js
@@ -0,0 +1,5 @@
+/**
+ * Created by palash on 4/13/2017.
+ */
+
+alert(1)
---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Riad
+ * Date: 4/13/2017
+ * Time: 6:45 PM
+ */
+phpinfo();
+?>
+<script src="test.js"></script>
